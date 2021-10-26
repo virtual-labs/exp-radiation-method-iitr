bdc6e0a5c691ac10dcab0ad17d540c0607a0b1c3
jnipun1994 <91589495+jnipun1994@users.noreply.github.com>
2021-10-26 11:17:41 +0530

    Update main.js

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -58,36 +58,36 @@ function showSlides() {
 //SCALE FUNCTIONS
 function show1()  
    {  
-    setTimeout(function(){ document.getElementById("alidade_img").src="images/a1.png"; }, 400);
-    setTimeout(function(){  document.getElementById("alidade_img").src="images/a2.png"; }, 800);
+    setTimeout(function(){ document.getElementById("alidade_img").src="images/a1.PNG"; }, 400);
+    setTimeout(function(){  document.getElementById("alidade_img").src="images/a2.PNG"; }, 800);
    }
    function show2()  
    {  
-    setTimeout(function(){ document.getElementById("alidade_img").src="images/b2.png"; }, 400);
-    setTimeout(function(){  document.getElementById("alidade_img").src="images/b1.png"; }, 800);
+    setTimeout(function(){ document.getElementById("alidade_img").src="images/b2.PNG"; }, 400);
+    setTimeout(function(){  document.getElementById("alidade_img").src="images/b1.PNG"; }, 800);
    }
    function show3()  
    {  
-    setTimeout(function(){ document.getElementById("alidade_img").src="images/c1.png"; }, 400);
-    setTimeout(function(){  document.getElementById("alidade_img").src="images/c2.png"; }, 800);
+    setTimeout(function(){ document.getElementById("alidade_img").src="images/c1.PNG"; }, 400);
+    setTimeout(function(){  document.getElementById("alidade_img").src="images/c2.PNG"; }, 800);
    }
    function show4()  
    {  
-    setTimeout(function(){ document.getElementById("alidade_img").src="images/d1.png"; }, 400);
-    setTimeout(function(){  document.getElementById("alidade_img").src="images/d2.png"; }, 800);
+    setTimeout(function(){ document.getElementById("alidade_img").src="images/d1.PNG"; }, 400);
+    setTimeout(function(){  document.getElementById("alidade_img").src="images/d2.PNG"; }, 800);
    }
    function show5()  
    {  
-    setTimeout(function(){ document.getElementById("alidade_img").src="images/e1.png"; }, 400);
-    setTimeout(function(){  document.getElementById("alidade_img").src="images/e2.png"; }, 800);
+    setTimeout(function(){ document.getElementById("alidade_img").src="images/e1.PNG"; }, 400);
+    setTimeout(function(){  document.getElementById("alidade_img").src="images/e2.PNG"; }, 800);
    }
 
    //OBJECT FUNCTIONS
 function tree()
 {
         
-        setTimeout(function(){ document.getElementById("alidade_img").src="images/a3.png"; }, 400);
-        setTimeout(function(){  document.getElementById("alidade_img").src="images/a4.png"; }, 800);
+        setTimeout(function(){ document.getElementById("alidade_img").src="images/a3.PNG"; }, 400);
+        setTimeout(function(){  document.getElementById("alidade_img").src="images/a4.PNG"; }, 800);
         setTimeout(function(){ document.getElementById("drawobjimg").style.transform="translate(-70px,-45px)scale(1)rotateY(150deg)"; }, 400);
         setTimeout(function(){  document.getElementById("drawobjimg").style.transform="translate(0px,0px)"; }, 800);
         setInterval(() => {document.getElementById("lineobj1img").style.display="inline"}, 400);
@@ -96,8 +96,8 @@ function tree()
 function tower()
 {
    
-    setTimeout(function(){ document.getElementById("alidade_img").src="images/b3.png"; }, 400);
-    setTimeout(function(){  document.getElementById("alidade_img").src="images/b4.png"; }, 800);
+    setTimeout(function(){ document.getElementById("alidade_img").src="images/b3.PNG"; }, 400);
+    setTimeout(function(){  document.getElementById("alidade_img").src="images/b4.PNG"; }, 800);
     setTimeout(function(){ document.getElementById("drawobjimg").style.transform="translate(-40px,-65px)scale(2)rotateY(80deg)"; }, 400);
     setTimeout(function(){  document.getElementById("drawobjimg").style.transform="translate(0px,0px)"; }, 800);
     setInterval(() => {document.getElementById("lineobj2img").style.display="inline"}, 400);
@@ -107,8 +107,8 @@ function tower()
 function solar()
 {
    
-    setTimeout(function(){ document.getElementById("alidade_img").src="images/c3.png"; }, 400);
-    setTimeout(function(){  document.getElementById("alidade_img").src="images/c4.png"; }, 800);
+    setTimeout(function(){ document.getElementById("alidade_img").src="images/c3.PNG"; }, 400);
+    setTimeout(function(){  document.getElementById("alidade_img").src="images/c4.PNG"; }, 800);
     setTimeout(function(){ document.getElementById("drawobjimg").style.transform="translate(-25px,-65px)scale(2)rotateY(60deg)"; }, 400);
     setTimeout(function(){  document.getElementById("drawobjimg").style.transform="translate(0px,0px)"; }, 800);
     setInterval(() => {document.getElementById("lineobj3img").style.display="inline"}, 400);
@@ -118,8 +118,8 @@ function solar()
 function house()
 {
    
-    setTimeout(function(){ document.getElementById("alidade_img").src="images/d3.png"; }, 400);
-    setTimeout(function(){  document.getElementById("alidade_img").src="images/d4.png"; }, 800);
+    setTimeout(function(){ document.getElementById("alidade_img").src="images/d3.PNG"; }, 400);
+    setTimeout(function(){  document.getElementById("alidade_img").src="images/d4.PNG"; }, 800);
     setTimeout(function(){ document.getElementById("drawobjimg").style.transform="translate(-20px,-45px)scale(1)rotateY(359deg)"; }, 400);
     setTimeout(function(){  document.getElementById("drawobjimg").style.transform="translate(0px,0px)"; }, 800);
     setInterval(() => {document.getElementById("lineobj4img").style.display="inline"}, 400);
@@ -128,8 +128,8 @@ function house()
 function street()
 {
    
-    setTimeout(function(){ document.getElementById("alidade_img").src="images/e3.png"; }, 400);
-    setTimeout(function(){  document.getElementById("alidade_img").src="images/e4.png"; }, 800);
+    setTimeout(function(){ document.getElementById("alidade_img").src="images/e3.PNG"; }, 400);
+    setTimeout(function(){  document.getElementById("alidade_img").src="images/e4.PNG"; }, 800);
     setTimeout(function(){ document.getElementById("drawobjimg").style.transform="translate(-20px,-25px)scale(1)rotateX(-150deg)"; }, 400);
     setTimeout(function(){  document.getElementById("drawobjimg").style.transform="translate(0px,0px)"; }, 800);
     setInterval(() => {document.getElementById("lineobj5img").style.display="inline"}, 400);
