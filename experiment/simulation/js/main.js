63af2b716782dd236dd2a5f06ac0fa565c43730f
jnipun1994 <91589495+jnipun1994@users.noreply.github.com>
2021-10-28 11:04:17 +0530

    Update main.js

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -58,39 +58,54 @@ function showSlides() {
 //SCALE FUNCTIONS
 function show1()  
    {  
-    setTimeout(function(){ document.getElementById("alidade_img").src="images/a1.PNG"; }, 400);
-    setTimeout(function(){  document.getElementById("alidade_img").src="images/a2.PNG"; }, 800);
+    setTimeout(function(){ document.getElementById("alidade_img").src="images/a1.png"; }, 400);
+    setTimeout(function(){  document.getElementById("alidade_img").src="images/a2.png"; }, 800);
    }
    function show2()  
    {  
-    setTimeout(function(){ document.getElementById("alidade_img").src="images/b2.ONG"; }, 400);
-    setTimeout(function(){  document.getElementById("alidade_img").src="images/b1.PNG"; }, 800);
+    setTimeout(function(){ document.getElementById("alidade_img").src="images/b2.png"; }, 400);
+    setTimeout(function(){  document.getElementById("alidade_img").src="images/b1.png"; }, 800);
    }
    function show3()  
    {  
-    setTimeout(function(){ document.getElementById("alidade_img").src="images/c1.PNG"; }, 400);
-    setTimeout(function(){  document.getElementById("alidade_img").src="images/c2.PNG"; }, 800);
+    setTimeout(function(){ document.getElementById("alidade_img").src="images/c1.png"; }, 400);
+    setTimeout(function(){  document.getElementById("alidade_img").src="images/c2.png"; }, 800);
    }
    function show4()  
    {  
-    setTimeout(function(){ document.getElementById("alidade_img").src="images/d1.PNG"; }, 400);
-    setTimeout(function(){  document.getElementById("alidade_img").src="images/d2.PNG"; }, 800);
+    setTimeout(function(){ document.getElementById("alidade_img").src="images/d1.png"; }, 400);
+    setTimeout(function(){  document.getElementById("alidade_img").src="images/d2.png"; }, 800);
    }
    function show5()  
    {  
-    setTimeout(function(){ document.getElementById("alidade_img").src="images/e1.PNG"; }, 400);
-    setTimeout(function(){  document.getElementById("alidade_img").src="images/e2.PNG"; }, 800);
+    setTimeout(function(){ document.getElementById("alidade_img").src="images/e1.png"; }, 400);
+    setTimeout(function(){  document.getElementById("alidade_img").src="images/e2.png"; }, 800);
    }
 
    //OBJECT FUNCTIONS
 function tree()
 {
-        
         setTimeout(function(){ document.getElementById("alidade_img").src="images/a3.png"; }, 400);
         setTimeout(function(){  document.getElementById("alidade_img").src="images/a4.png"; }, 800);
         setTimeout(function(){ document.getElementById("drawobjimg").style.transform="translate(-70px,-45px)scale(1)rotateY(150deg)"; }, 400);
         setTimeout(function(){  document.getElementById("drawobjimg").style.transform="translate(0px,0px)"; }, 800);
         setInterval(() => {document.getElementById("lineobj1img").style.display="inline"}, 400);
+         // add eventlistener for click to window:
+ window.addEventListener('click', (event) => {
+  // event.target is the element having been clicked
+  // was a button .obj clicked?
+  if (event.target.classList.contains('obj')) {
+      // add class "clicked" to the button:
+      event.target.classList.add('clicked');
+      // are all buttons clicked now?
+      if (!document.querySelector('.obj:not(.clicked)')) {
+          // make next button visible:
+          document.getElementById("result").style.display = "inline";
+          swal("Click on the RESULT Button to proceed further");
+          
+      }
+  }
+});
         
 }
 function tower()
@@ -101,6 +116,21 @@ function tower()
     setTimeout(function(){ document.getElementById("drawobjimg").style.transform="translate(-40px,-65px)scale(2)rotateY(80deg)"; }, 400);
     setTimeout(function(){  document.getElementById("drawobjimg").style.transform="translate(0px,0px)"; }, 800);
     setInterval(() => {document.getElementById("lineobj2img").style.display="inline"}, 400);
+    window.addEventListener('click', (event) => {
+      // event.target is the element having been clicked
+      // was a button .obj clicked?
+      if (event.target.classList.contains('obj')) {
+          // add class "clicked" to the button:
+          event.target.classList.add('clicked');
+          // are all buttons clicked now?
+          if (!document.querySelector('.obj:not(.clicked)')) {
+              // make next button visible:
+              document.getElementById("result").style.display = "inline";
+              swal("Click on the RESULT Button to proceed further");
+              
+          }
+      }
+    });
    
 }
 
@@ -112,6 +142,21 @@ function solar()
     setTimeout(function(){ document.getElementById("drawobjimg").style.transform="translate(-25px,-65px)scale(2)rotateY(60deg)"; }, 400);
     setTimeout(function(){  document.getElementById("drawobjimg").style.transform="translate(0px,0px)"; }, 800);
     setInterval(() => {document.getElementById("lineobj3img").style.display="inline"}, 400);
+    window.addEventListener('click', (event) => {
+      // event.target is the element having been clicked
+      // was a button .obj clicked?
+      if (event.target.classList.contains('obj')) {
+          // add class "clicked" to the button:
+          event.target.classList.add('clicked');
+          // are all buttons clicked now?
+          if (!document.querySelector('.obj:not(.clicked)')) {
+              // make next button visible:
+              document.getElementById("result").style.display = "inline";
+              swal("Click on the RESULT Button to proceed further");
+              
+          }
+      }
+    });
       
 }
 
@@ -123,6 +168,21 @@ function house()
     setTimeout(function(){ document.getElementById("drawobjimg").style.transform="translate(-20px,-45px)scale(1)rotateY(359deg)"; }, 400);
     setTimeout(function(){  document.getElementById("drawobjimg").style.transform="translate(0px,0px)"; }, 800);
     setInterval(() => {document.getElementById("lineobj4img").style.display="inline"}, 400);
+    window.addEventListener('click', (event) => {
+      // event.target is the element having been clicked
+      // was a button .obj clicked?
+      if (event.target.classList.contains('obj')) {
+          // add class "clicked" to the button:
+          event.target.classList.add('clicked');
+          // are all buttons clicked now?
+          if (!document.querySelector('.obj:not(.clicked)')) {
+              // make next button visible:
+              document.getElementById("result").style.display = "inline";
+              swal("Click on the RESULT Button to proceed further");
+              
+          }
+      }
+    });
    
 }
 function street()
@@ -133,19 +193,21 @@ function street()
     setTimeout(function(){ document.getElementById("drawobjimg").style.transform="translate(-20px,-25px)scale(1)rotateX(-150deg)"; }, 400);
     setTimeout(function(){  document.getElementById("drawobjimg").style.transform="translate(0px,0px)"; }, 800);
     setInterval(() => {document.getElementById("lineobj5img").style.display="inline"}, 400);
+    window.addEventListener('click', (event) => {
+      // event.target is the element having been clicked
+      // was a button .obj clicked?
+      if (event.target.classList.contains('obj')) {
+          // add class "clicked" to the button:
+          event.target.classList.add('clicked');
+          // are all buttons clicked now?
+          if (!document.querySelector('.obj:not(.clicked)')) {
+              // make next button visible:
+              document.getElementById("result").style.display = "inline";
+              swal("Click on the RESULT Button to proceed further");
+              
+          }
+      }
+    });
     
 }
-const imgs = Array.from(document.querySelectorAll('.obj'));
-        // Add event listener for document:
-        document.addEventListener('click', event => {
-            // Has one of the images been clicked?
-            if (event.target.classList.contains('obj')) {
-                // Add class indicating that is has been clicked:
-                event.target.classList.add('is-clicked');
-                // Check if all images have been clicked:
-                if (!imgs.find(item => !item.classList.contains('is-clicked'))) {
-                    // Make button visible:
-                    document.querySelector('#result').style.display='inline';
-                }
-            }
-        });
+
